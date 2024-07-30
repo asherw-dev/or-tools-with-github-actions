9bd8822815d9eb11507758cb85f31f4cac6a3296
Asher Weinberg <AsherW@BoldIntel.com>
2024-07-30 18:44:46 +0300

    Add manual workflow

--- src/test.py
+++ src/test.py
@@ -1,4 +1,5 @@
 import unittest
+import argparse
 
 # Example function to be tested
 def add(a, b):
@@ -7,18 +8,18 @@ def add(a, b):
 # Define the test case
 class TestAddFunction(unittest.TestCase):
 
-    def test_add_positive_numbers(self):
-        self.assertEqual(add(1, 2), 3)
-        self.assertEqual(add(10, 5), 15)
+    def test_add(self):
+        with open(args.file, 'r') as file:
+            numbers = file.readline().split()
+            num1 = int(numbers[0])
+            num2 = int(numbers[1])
+            result = add(num1, num2)
+            expected_result = num1 + num2
+            self.assertEqual(result, expected_result)
 
-    def test_add_negative_numbers(self):
-        self.assertEqual(add(-1, -1), -2)
-        self.assertEqual(add(-10, -5), -15)
-
-    def test_add_mixed_numbers(self):
-        self.assertEqual(add(-1, 1), 0)
-        self.assertEqual(add(-10, 5), -5)
-
-# Run the tests
 if __name__ == '__main__':
-    unittest.main()
+    parser = argparse.ArgumentParser(description="Run tests on the add function.")
+    parser.add_argument('--file', required=True, help='Path to the file containing numbers')
+    args = parser.parse_args()
+
+    unittest.main(argv=['first-arg-is-ignored'], exit=False)
